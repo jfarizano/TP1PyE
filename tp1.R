dda2eb9e971a5c6e92a8e27239ce7387efcd4b7d
Jfarizano <jifarizano@hotmail.com>
2020-04-06 18:58:53 +0000

    terminados graficos simples

--- tp1.R
+++ tp1.R
@@ -18,7 +18,7 @@ tablasAltura <- function(Altura, fuente) {
   axis(side = 2, at = seq(0, 100, 10))
   
   # Polígono acumulativo (FALTA QUE APAREZCA LE NRO 35 EN EL EJE X)
-  plot(frecRelAcum, type = "l", main = tit, sub = fuente, xlab = "Altura (en metros)", ylab = "Frecuencia relativa acumulada", xaxt = "n", font.lab = 2)
+  plot(c(frecRelAcum, 1), type = "l", main = tit, sub = fuente, xlab = "Altura (en metros)", ylab = "Frecuencia relativa acumulada", xaxt = "n", font.lab = 2, xlim = c(1, 8))
   axis(side = 1, at = (1:8), labels = seq(0, 35, 5))
   abline(h = seq(0, 1, 0.2), lty = 3)
 }
@@ -44,15 +44,56 @@ tablasDiametro <- function(Diametro, fuente){
   axis(side = 2, at = seq(0, 150, 10))
   
   # Polígono acumulativo.
-  plot(frecRelAcum, type = "l", main = tit, sub = fuente, xlab = "Diámetro (en cm)", ylab = "Frecuencia relativa acumulada", xaxt = "n", font.lab = 2)
+  plot(c(frecRelAcum, 1), type = "l", main = tit, sub = fuente, xlab = "Diámetro (en cm)", ylab = "Frecuencia relativa acumulada", xaxt = "n", font.lab = 2)
   axis(side = 1, at = (1:8), labels = seq(0, 140, 20))
   abline(h = seq(0, 1, 0.2), lty = 3)
 }
 
-# tablasEspecie <- function(Especie, fuente) {
-#   frecAbs <- table(Especie)
-#   barplot(frecAbs, ylim = c(0, 70), col = "green")
-# }
+tablasInclinacion <- function(Inclinacion, fuente) {
+  frecAbs <- table(cut(Inclinacion, seq(0, 45, 9), right = "FALSE"))
+  frecRel <- round(frecAbs/length(Inclinacion), digits = 4)
+  frecAbsAcum = cumsum(frecAbs)
+  frecRelAcum = round(cumsum(frecAbs/length(Inclinacion)), digits = 4)
+  tabla_inclinacion <<- cbind(frecAbs, frecRel, frecAbs, frecAbsAcum, frecRelAcum)
+  
+  tit <- "INCLINACIÓN DE LOS ÁRBOLES"
+  b <- seq(0, 45, 9)
+  hist(Inclinacion, main = tit, xlab = "Inclinación (en grados)", ylab = "Frecuencia absoluta", 
+       xlim = c(0, 45), ylim = c(0, 320), sub = fuente, col = "lightblue", 
+       breaks = b, xaxt = "n", yaxt = "n", right = FALSE, font.lab = 2)
+  axis(side = 1, at = b)
+  axis(side = 2, at = seq(0, 320, 20))
+  
+  # Polígono acumulativo.
+  plot(c(frecRelAcum, 1), type = "l", main = tit, sub = fuente, xlim = c(1,6), ylim = c(0.85, 1), xlab = "Inclinación (en grados)", ylab = "Frecuencia relativa acumulada", xaxt = "n", font.lab = 2)
+  axis(side = 1, at = (1:6), labels = seq(0, 45, 9))
+  abline(h = seq(0.85, 1, 0.05), lty = 3)
+}
+
+tablasEspecie <- function(Especie, fuente) {
+   frecAbs <- table(Especie)
+   
+   tit <- "ÁRBOLES SEGÚN SU ESPECIE"
+   barplot(frecAbs, ylim = c(0, 70), col = "green", main = tit, sub = fuente, xlab = "Especie", ylab = "Frecuencia absoluta", font.lab = 2)
+}
+
+tablasOrigen <- function(Origen, fuente) {
+  frecAbs <- table(Origen)
+  porcentajes <- round((frecAbs / length(Origen)) * 100, digits = 2)
+  lbls <- c("Exótico", "Nativo/Autóctono")
+  lbls <- paste(lbls, porcentajes)
+  lbls <- paste(lbls, "%")
+  
+  tit <- "ÁRBOLES SEGÚN SU ORIGEN"
+  pie(frecAbs, labels = lbls, main = tit, sub = fuente, col = c("purple", "cyan"))
+}
+
+tablasBrotes <- function(Brotes, fuente) {
+  frecAbs <- table(Brotes)
+  
+  tit <- "BROTES CRECIDOS POR ÁRBOL\nEN EL ÚLTIMO AÑO"
+  plot(frecAbs, type = "h", main = tit, sub = fuente, xlab = "Cantidad de brotes", ylab = "Frecuencia absoluta", font.lab = 2)
+}
 
 # Variable a analizar: 
 
@@ -64,7 +105,10 @@ leerDatos <- function() {
   fuente <- "Fuente: Censo Forestal Urbano Público, Ciudad de Buenos Aires, año 2011"
   tablasAltura(Altura, fuente)
   tablasDiametro(Diametro, fuente)
-  # tablasEspecie(Especie, fuente)
+  tablasInclinacion(Inclinacion, fuente)
+  tablasEspecie(Especie, fuente)
+  tablasOrigen(Origen, fuente)
+  tablasBrotes(Brotes, fuente)
 }
 
 leerDatos()
